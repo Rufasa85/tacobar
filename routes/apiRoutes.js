6a254603c15fc9779e1be15b4e255b16dfaaa6bc
Joe Rehfuss <joe.rehfuss@gmail.com>
2020-10-07 14:45:36 -0700

    modulariztion

---
+++ routes/apiRoutes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+
+
+//build data structures
+const tableDataObj = require("../data/tables.js")
+const waitlistDataObj = require("../data/waitlist.js")
+// array of reservations (tables)
+let tableData = tableDataObj.tableData;
+//array of waitlist
+let waitlistData = waitlistDataObj.waitlistData;;
+// GET route for table data
+
+router.get("/tables",function(req,res){
+    res.json(tableData)
+})
+// GET route for waitlist data
+router.get("/waitlist",function(req,res){
+    res.json(waitlistData)
+})
+//GET route to clear table data
+router.get("/clear",function(req,res){
+    tableData=[];
+    waitlistData=[];
+    res.send("DELETED!")
+})
+
+//POST route to add a reservation to either tables or waitlist
+router.post("/tables",function(req,res){
+    let hasTable = false;
+    if(tableData.length<5){
+        tableData.push(req.body)
+        hasTable=true
+    }else {
+        waitlistData.push(req.body)
+    }
+    res.json(hasTable)
+})
+
+module.exports = router;
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+
+router.get("/",function(req,res){
+    res.sendFile(path.join(__dirname,"../views/index.html"))
+})
+ //GET route for homepage
+router.get("/reserve",function(req,res){
+    res.sendFile(path.join(__dirname,"../views/reserve.html"))
+})
+
+module.exports = router;
